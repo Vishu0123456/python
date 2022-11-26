9c4480f3a35873ac7e478d21352ae82ccae6c45d
Vishu0123456 <115339745+Vishu0123456@users.noreply.github.com>
2022-11-26 12:09:44 +0530

    Add files via upload

---
+++ 02_operators.py
@@ -0,0 +1,73 @@
+a = 3
+b = 4
+c = 5
+d = 10
+e = 15
+
+# Arithmetic operators
+print("The value of 3+4+10+15+5 ",3+4+10+15+5)
+print("The value of 3-4-5-10-15 ",3-4-5-10-15)
+print("The value of 3*4 ",3*4)
+print("The value of 3/4 ",3/4)
+
+#Assigment operators
+a = 3
+a += 5
+print(a)
+
+# comparison operators
+
+b = (10>20)
+print(b)
+c = (25>12)
+print(c)
+
+d = (14!=7)
+print(d)
+
+#logicall operator 
+
+bool1 = True
+bool2 = False
+print("the value of bool1 and bool2 is",(bool1 and bool2))
+print("the value of bool1 or bool2 is", (bool1 or bool2))
+print("the value of not bool2 is",(not bool2))
+print(3==4)
+
+a = True
+b = False
+print("the value of a and b is",(a and b))
+print("the value of a or b is", (a or b))
+print("the value of a not is",(not a))
+print("the value of not b is",(not b))
+
+a = 3
+b = 4
+print("the value of a and b is",(a and b))
+print("the value of a or b is", (a or b))
+print ("the value of not b is",(not b))
+
+
+
+
+
+
+
+
+
+x = (input("x:"))
+y = (input("y:"))
+z = (x+y)
+print(x)
+print(y)
+print("x%s,y=%s,z=%s"%(x,y,z))
+
+
+#formatting.property
+
+
+a = int(input("a:"))
+b = int(input("b:"))
+c = a+b
+print(a)
+print(b)
